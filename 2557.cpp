cd63134a21be04c7e4f364374cf9c57a275f8488
BonJunKu <ku96kbj@naver.com>
2021-01-21 22:28:57 +0900

    Update 2557.cpp

--- 2557.cpp
+++ 2557.cpp
@@ -1,7 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include<stdio.h>
 int main() {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d", a+b);
+	printf("Hello World!");
 }
+
+//sol)
+//#import<ios>
+//main() { puts("Hello World!"); }
