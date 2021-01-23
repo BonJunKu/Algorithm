fee745ebbc648f8c0879355ef38ea1dbfb40faac
BonJunKu <ku96kbj@naver.com>
2021-01-23 13:46:53 +0900

    io
    io

---
+++ IO/2558.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main() {
+	int t, a, b;
+	printf("%d", 5);
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		printf("%d", t);
+		scanf("%d", &a);
+		scanf("%d", &b);
+		printf("%d", a + b);
+	}
+	return 0;
+}
