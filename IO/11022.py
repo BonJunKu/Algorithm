98aa662776c4d76bc4a281c3ac05a2ad710d0a02
BonJunKu <ku96kbj@naver.com>
2021-01-24 20:36:39 +0900

    io

--- IO/11022.py
+++ IO/11022.py
@@ -1,5 +1,3 @@
 import sys
-n=int(input())
-for i in range(n):
-    a,b=map(int,sys.stdin.readline().split())
-    print("Case #{}: {} + {} = {}".format(i+1,a,b,a+b))
+for i in sys.stdin(100):
+    print(i)
