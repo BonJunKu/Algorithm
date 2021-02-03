b55f62ada45e5c4c4389800db0d0f46c54912d08
BonJunKu <ku96kbj@naver.com>
2021-02-03 23:20:40 +0900

    Update 11724.py

--- Graph/11724.py
+++ Graph/11724.py
@@ -30,30 +30,33 @@ for i in range(1,N+1):
 print(count)
 
 '''
-
 import sys
 sys.setrecursionlimit(10000)
-def dfs(i,G,V,M):
-    V[i]=True
-    for j in range(1,N+1):
-        if V[j]==True or G[i][j]==0:
-            continue
-            dfs(j,G,V,N)
+def dfs(v):
+    visited[v]=True
+    for i in range(1,N+1):
+        if visited[i]==False and graph[v][i]==1:
+            dfs(i)
+'''
+1. visited 에 True 표시,
+2. 점 1부터 차례로 돌면서 한번도 온적 없는 점인데 이 점과 i번째 점이 연결되어있을경우
 
-N = 1001
-graph = [[0] * N for i in range(N)]
-visited = [0] * N
-U, V = map(int,input().split(' '))
-for i in range(V):
-    u, v = map(int, sys.stdin.readline().split())
+점=[] 별로 만드는 법이 있지만
+이중리스트가 더 빠른듯하다. 
+'''
 
-    graph[u][v] = 1
-    graph[v][u] = 1
-
-count = 0
-for i in range (1, U+1):
-    if visited[i] == 0:
-        dfs(i, graph, visited, U)
-        count += 1
-
+N,M=map(int,sys.stdin.readline().strip().split())
+graph=[[0]*(N+1) for _ in range(N+1)]
+visited=[False]*(N+1) #visited는 False
+for i in range(M):
+    x,y=map(int,sys.stdin.readline().strip().split())
+    graph[x][y]=1
+    graph[y][x]=1
+count =0
+for i in range(1,N+1):
+    if visited[i]==False:
+        dfs(i)
+        count+=1
 print(count)
+
+
